d3e8da6c81f88b04375fbc28ad458525676b8159
Raymond Camden <raymondcamden@gmail.com>
2025-03-10 13:06:34 -0500

    demos

---
+++ kg_sentiment_analysis/test1.py
@@ -0,0 +1,27 @@
+import os 
+import requests 
+import json 
+import urllib.parse
+
+token = os.environ.get("db_token")
+
+query = 'type:Article text:"xbox" language:"en" sentiment<=0 date>"2025-03-03" sortBy:date'
+
+apiCall = f"https://kg.diffbot.com/kg/v3/dql?type=query&token={token}&query={urllib.parse.quote(query)}&size=25"
+
+req = requests.get(apiCall)
+results = json.loads(req.content)
+print(f"Total results, {results['hits']}")
+
+for result in results["data"]:
+	print(result["entity"]["title"])
+	print(result["entity"]["date"]["str"])
+	print(result["entity"]["summary"])
+	if "author" in result["entity"]:
+		print(result["entity"]["author"])
+	print(result["entity"]["siteName"])
+	print(result["entity"]["pageUrl"])
+	print(result["entity"]["sentiment"])
+
+	print("------------------------------------")
+
---
+++ kg_sentiment_analysis/test2.py
@@ -0,0 +1,33 @@
+import os 
+import requests 
+import json 
+from datetime import datetime, timedelta
+import urllib.parse
+
+
+token = os.environ.get("db_token")
+
+today = datetime.now()
+lastWeek = today + timedelta(days=-7)
+fLastWeek = lastWeek.strftime("%Y-%m-%d")
+
+query = f'type:Article text:"xbox" language:"en" sentiment<=0 date>{fLastWeek} sortBy:date'
+
+apiCall = f"https://kg.diffbot.com/kg/v3/dql?type=query&token={token}&query={urllib.parse.quote(query)}&size=25"
+
+req = requests.get(apiCall)
+results = json.loads(req.content)
+
+print(f"Total results, {results['hits']}")
+
+for result in results["data"]:
+	print(result["entity"]["title"])
+	print(result["entity"]["date"]["str"])
+	print(result["entity"]["summary"])
+	if "author" in result["entity"]:
+		print(result["entity"]["author"])
+	print(result["entity"]["siteName"])
+	print(result["entity"]["pageUrl"])
+	print(result["entity"]["sentiment"])
+
+	print("------------------------------------")
